0841e251cdb8cb6db0541ae9d16c8a7b2b044f3a
jbyh <jessebharrick@gmail.com>
2025-11-25 22:55:33 -0600

    Updated SETUP.md

--- next.config.js
+++ next.config.js
@@ -4,10 +4,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
